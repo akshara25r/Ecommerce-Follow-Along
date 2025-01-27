edb92fcfcef8a84d0a07c4646f4a44024bdab51e
Akshara <aksharar02@gmail.com>
2025-01-27 21:46:50 +0530

    milestone 3 added

--- backend/app.js
+++ backend/app.js
@@ -1,43 +1,27 @@
-const express= require("express");
-const app = express()
+const express = require("express");
+const app = express();
+const ErrorHandler = require("./middleware/error");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
 
-if(process.env.NODE_ENV !=="PRODUCTION"){
+
+
+app.use(express.json());
+app.use(cookieParser());
+app.use("/",express.static("uploads"));
+app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+// config
+if (process.env.NODE_ENV !== "PRODUCTION") {
     require("dotenv").config({
-        path:"backend/config/.env"
+      path: "backend/config/.env",
     });
 };
+//import Routes
+const user = require("./controller/user");
 
-// app.use(ErrorHandler);
-module.exports =app;
+app.use("/api/v2/user", user);
 
+// it's for ErrorHandling
+app.use(ErrorHandler);
 
-// const express = require("express");
-// const app = express();
-// const ErrorHandler = require("./middleware/error");
-// const cookieParser = require("cookie-parser");
-// const bodyParser = require("body-parser");
-
-
-
-
-
-
-// app.use(express.json());
-// app.use(cookieParser());
-// app.use("/",express.static("uploads"));
-// app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
-// // config
-// if (process.env.NODE_ENV !== "PRODUCTION") {
-//     require("dotenv").config({
-//       path: "backend/config/.env",
-//     });
-// };
-// //import Routes
-// const user = require("./controller/user");
-
-// app.use("/api/v2/user", user);
-
-// // it's for ErrorHandling
-// app.use(ErrorHandler);
-
-// module.exports = app;
+module.exports = app;
