eb77dc12c9ee91484bcf69d107a5d258f8f73e13
Akshara <aksharar02@gmail.com>
2025-02-13 22:58:47 +0530

    milestone12 is added

--- frontend/src/Routes/Routes.jsx
+++ frontend/src/Routes/Routes.jsx
@@ -3,5 +3,6 @@ import Login from "../pages/Login.jsx";
 import Signup from "../pages/Signup.jsx";
 import Home from "../pages/Home.jsx"
 import CreateProduct from "../pages/createProduct.jsx";
-export { Login,Signup,Home, CreateProduct}
+import MyProducts from "../pages/myProducts.jsx";
+export { Login,Signup,Home, CreateProduct, MyProducts}
 
